e8780c832850c3cf77052042e68c031814f2e51f
Daniel Matthewman <djm321@hotmail.co.uk>
2026-05-21 22:59:07 +0100

    added methods to populate registration form

--- Fixtures/PageObjectFixtures.ts
+++ Fixtures/PageObjectFixtures.ts
@@ -2,11 +2,13 @@ import { test as base } from "@playwright/test";
 import { SignupLoginPage } from "../pages/signupLoginPage";
 import { Navigation } from "../pages/navigation/navigation";
 import { HomePage } from "../pages/homePage";
+import { AccountInformationPage } from "../pages/accountInformationPage";
 
 export type PageObjectFixtures = {
   readonly navigation: Navigation;
   readonly homePage: HomePage;
   readonly signupLoginPage: SignupLoginPage;
+  readonly accountInformationPage: AccountInformationPage;
 };
 
 export const test = base.extend<PageObjectFixtures>({
@@ -21,6 +23,10 @@ export const test = base.extend<PageObjectFixtures>({
   signupLoginPage: async ({ page }, use) => {
     const signupLoginPage = new SignupLoginPage(page);
     await use(signupLoginPage);
+  },
+  accountInformationPage: async ({ page }, use) => {
+    const accountInformationPage = new AccountInformationPage(page);
+    await use(accountInformationPage);
   },
 });
 
--- tests/registerNewUser.e2e.ts
+++ tests/registerNewUser.e2e.ts
@@ -1,14 +1,58 @@
 import { expect, test } from "../Fixtures/PageObjectFixtures";
-import { createRandomUser } from "../helpers/createRandomData";
+import { createRandomCustomer } from "../helpers/createRandomData";
 
-const randomCustomer = createRandomUser();
+const randomCustomer = createRandomCustomer();
 
-test("Register New User", async ({ homePage, navigation, signupLoginPage }) => {
-  await homePage.navigateToHomePage();
-  await navigation.acceptCookies();
-  await navigation.signupLoginLink.click();
-  await signupLoginPage.populateSignupForm(
-    randomCustomer.fullName,
-    randomCustomer.email,
-  );
+test("Register New User", async ({
+  homePage,
+  navigation,
+  signupLoginPage,
+  accountInformationPage,
+}) => {
+  await test.step("Given I navigate to Signup/Login page", async () => {
+    await homePage.navigateToHomePage();
+    await navigation.acceptCookies();
+  });
+
+  await test.step("When I click the signup/login link and populate", async () => {
+    await navigation.signupLoginLink.click();
+  });
+
+  await test.step("And I populate the signup form", async () => {
+    await signupLoginPage.populateSignupForm(
+      `${randomCustomer.firstName} ${randomCustomer.lastName}`,
+      randomCustomer.email,
+    );
+  });
+
+  await test.step("Then I should see the enter account information page with name and emailk pre populated", async () => {
+    await expect(accountInformationPage.nameField).toHaveValue(
+      `${randomCustomer.firstName} ${randomCustomer.lastName}`,
+    );
+    await expect(accountInformationPage.emailField).toHaveValue(
+      randomCustomer.email,
+    );
+  });
+
+  await test.step("When I populate the account information form and submit", async () => {
+    await accountInformationPage.populateAccountInformationForm({
+      gender: randomCustomer.gender,
+      password: randomCustomer.password,
+      dayOfBirth: randomCustomer.dayOfBirth,
+      monthOfBirth: randomCustomer.monthOfBirth,
+      yearOfBirth: randomCustomer.yearOfBirth,
+      signUpForNewsletter: randomCustomer.signUpForNewsletter,
+      receiveSpecialOffers: randomCustomer.receiveSpecialOffers,
+      firstName: randomCustomer.firstName,
+      lastName: randomCustomer.lastName,
+      company: randomCustomer.company,
+      addressLine1: randomCustomer.addressLine1,
+      addressLine2: randomCustomer.addressLine2,
+      country: randomCustomer.country,
+      state: randomCustomer.state,
+      city: randomCustomer.city,
+      zipcode: randomCustomer.zipcode,
+      mobileNumber: randomCustomer.mobileNumber,
+    });
+  });
 });
